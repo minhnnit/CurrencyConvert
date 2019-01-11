c15de36ec6be9572356d2a361efd6bc7c2243239
Nhu Minh <minhnnit>
2019-01-11 15:15:10 +0700

    edit section convert currencies section

--- resources/views/convert-number-index.blade.php
+++ resources/views/convert-number-index.blade.php
@@ -414,106 +414,29 @@ Convert" buttons="danger info primary success warning inverse link" class="btn b
                                 class="color-blue">{{!empty($data["arraySum"]) ? $data["arraySum"] : '1'}}</span>
                     </div>
                 </div>
-                {{--<div class="convert-currency">
-                    <h3>CONVERT CURRENCY</h3>
-                    <form method="post" id="currency-form">
-                        <div class="form-group">
-                            <label>Amount</label>
-                            <input type="number" placeholder="Currency" name="amount" id="amount" class="form-control inputCurrency" value="1"/>
-                            <label>From</label>
-                            <select name="from_currency" class="inputCurrency form-control">
-                                <option value="INR">Indian Rupee</option>
-                                <option value="USD" selected="1">US Dollar</option>
-                                <option value="AUD">Australian Dollar</option>
-                                <option value="EUR">Euro</option>
-                                <option value="EGP">Egyptian Pound</option>
-                                <option value="CNY">Chinese Yuan</option>
-                                <option value="GBP">British Pound</option>
-                                <option value="CAD">Canadian Dollar</option>
-                                <option value="AFN">Afghanistan Afghani</option>
-                                <option value="ARS">Argentine Peso</option>
-                                <option value="BSD">Bahamian Dollar</option>
-                                <option value="BRL">Brazilian Real</option>
-                                <option value="BND">Brunei Dollar</option>
-                                <option value="KHR">Cambodian Riel</option>
-                                <option value="CFP">Central Pacific Franc</option>
-                                <option value="COP">Colombian Peso</option>
-                                <option value="FRF">French Franc</option>
-                                <option value="DEM">German Mark</option>
-                                <option value="HKD">Hong Kong Dollar</option>
-                                <option value="ISK">Iceland Krona</option>
-                                <option value="IDR">Indonesian Rupiah</option>
-                                <option value="JPY">Japanese Yen</option>
-                                <option value="LAK">Lao Kip</option>
-                                <option value="MOP">Macau Pataca</option>
-                                <option value="MXN">Mexican Peso</option>
-                                <option value="PHP">Philippine Peso</option>
-                                <option value="VND">Vietnamese Dong</option>
-                                <option value="THB">Thai Baht</option>
-                                <option value="TWD">Taiwan Dollar</option>
-                                <option value="SEK">Swedish Krona</option>
-
-                            </select>
-                            <label>To</label>
-                            <select name="to_currency" class="inputCurrency toCurrency form-control">
-                                <option value="INR" selected="1">Indian Rupee</option>
-                                <option value="USD">US Dollar</option>
-                                <option value="AUD">Australian Dollar</option>
-                                <option value="EUR">Euro</option>
-                                <option value="EGP">Egyptian Pound</option>
-                                <option value="CNY">Chinese Yuan</option>
-                                <option value="GBP">British Pound</option>
-                                <option value="CAD">Canadian Dollar</option>
-                                <option value="AFN">Afghanistan Afghani</option>
-                                <option value="ARS">Argentine Peso</option>
-                                <option value="ARS">Argentine Peso</option>
-                                <option value="BSD">Bahamian Dollar</option>
-                                <option value="BRL">Brazilian Real</option>
-                                <option value="BND">Brunei Dollar</option>
-                                <option value="KHR">Cambodian Riel</option>
-                                <option value="CFP">Central Pacific Franc</option>
-                                <option value="COP">Colombian Peso</option>
-                                <option value="FRF">French Franc</option>
-                                <option value="DEM">German Mark</option>
-                                <option value="HKD">Hong Kong Dollar</option>
-                                <option value="ISK">Iceland Krona</option>
-                                <option value="IDR">Indonesian Rupiah</option>
-                                <option value="JPY">Japanese Yen</option>
-                                <option value="LAK">Lao Kip</option>
-                                <option value="MOP">Macau Pataca</option>
-                                <option value="MXN">Mexican Peso</option>
-                                <option value="PHP">Philippine Peso</option>
-                                <option value="VND">Vietnamese Dong</option>
-                                <option value="THB">Thai Baht</option>
-                                <option value="TWD">Taiwan Dollar</option>
-                                <option value="SEK">Swedish Krona</option>
-                            </select>
-                            =
-                            <input type="text" id="responseConvert" class="color-blue form-control inputCurrency" readonly>
-                           <div class="convert-button">
-                                <span name="convert" id="convert" class="btn btn-default"><b>Convert</b><i
-                                            class="fas fa-arrow-right"></i></span>
-                           </div>
-                        </div>
-                    </form>
-                </div>--}}
-                <div class="currency-to-text">
-                    <h3>CURRENCY TO TEXT</h3>
-                    <div class="lead">
-                        @if(!empty($listCurrency))
-                            @foreach($listCurrency as $v => $v_value)
-                                <div class="mg-bottom-5">
-                                    <input onclick="responsiveVoice.speak($('.{{$v}}').text());"
-                                           type='button'
-                                           value='🔊 Play'/><span
-                                            class="span-currency">{{$v}}:</span><span
-                                            class="color-blue span-currency {{$v}}">{{!empty($data["convertNumber"]) ? $data["convertNumber"] : 'one'}} {{$v_value}}</span>
-                                </div>
-                            @endforeach
-                        @endif
-                    </div>
-                    <div class="more more-lead">More...</div>
-                </div>
+                <section class="currency-res" tabindex="-1">
+                    <h3>Currencies To Audio</h3>
+                    <table class="table">
+                        <thead class="thead-color">
+                            <tr>
+                                <th>Currencies</th>
+                                <th>Currencies to text</th>
+                                <th>Audio</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @if(!empty($listCurrency))
+                                @foreach($listCurrency as $v => $v_value)
+                                    <tr>
+                                        <td><span class="span-currency"><b>{{$v}}</b></span></td>
+                                        <td><span class="span-currency {{$v}}">{{!empty($data["convertNumber"]) ? $data["convertNumber"] : 'one'}} {{$v_value}}</span></td>
+                                        <td><input class="speak-audio" onclick="responsiveVoice.speak($('.{{$v}}').text());" type='button' value='🔊'/></td>
+                                    </tr>
+                                @endforeach
+                            @endif
+                        </tbody>
+                    </table>
+                </section>
             </div>
         </div>
         <div class="col-lg-3 col-xs-12">
